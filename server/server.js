7f36dfbb0423556b0f7f4b86ccd5aa39ca6eb2e5
ICONIC_07 <souravp9057@gmail.com>
2023-08-05 18:11:02 +0530

    updated order route and server.js file

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ import conversationRoute from "./routes/conversation.route.js";
 import messageRoute from "./routes/message.route.js";
 import reviewRoute from "./routes/review.route.js";
 import authRoute from "./routes/auth.route.js"; 
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express()
 dotenv.config(); 
@@ -23,6 +25,10 @@ try {
   }
 };
 
+app.use(cors({origin:"http://localhost:5173", credentials:true }))
+app.use(express.json());
+app.use(cookieParser()); 
+
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/users", userRoute);
@@ -32,6 +38,12 @@ app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/reviews", reviewRoute);
 
+app.use((err,req,res,next)=>{
+  const errorStatus = err.status || 500
+  const errorMessage = err.message || "Something went wrong!"
+
+  return res.status(errorStatus).send(errorMessage);
+});
 
 app.listen(8800, () => {
     connect()
