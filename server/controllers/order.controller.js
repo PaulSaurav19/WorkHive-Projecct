3c5429ced6435a6ad4f3a2464e40e0773353a869
ICONIC_07 <souravp9057@gmail.com>
2023-08-18 13:00:20 +0530

    Fixed error in reviews and updated

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -9,7 +9,7 @@ export const intent = async (req, res, next) => {
 const gig = await Gig.findById(req.params.id)
 
 const paymentIntent = await stripe.paymentIntents.create({
-  amount: gig.price,
+  amount: gig.price * 100,
   currency: "inr",
   automatic_payment_methods: {
     enabled: true,
